209e477ed289f47bf5572c93e21ab8282e31568d
naguvan <naguvan@outlook.com>
2018-07-17 20:29:21 +0530

    react-mst-form-libary demo code.

---
+++ src/types/App.ts
@@ -0,0 +1,23 @@
+import { WithStyles } from '@material-ui/core';
+import { IFormConfig } from 'react-mst-form';
+
+export interface IAppStyles {
+    root: React.CSSProperties;
+    container: React.CSSProperties;
+    form: React.CSSProperties;
+    paper: React.CSSProperties;
+    submit: React.CSSProperties;
+}
+
+export interface IAppStyleProps extends WithStyles<keyof IAppStyles> {}
+
+export interface IAppProps {
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+export interface IAppStates {
+    width: string;
+    height: string;
+    config: IFormConfig;
+}
---
+++ src/types/Schema.ts
@@ -0,0 +1,19 @@
+import { WithStyles } from '@material-ui/core';
+import { IFormConfig } from 'react-mst-form';
+
+export interface ISchemaStyles {
+    paper: React.CSSProperties;
+}
+
+export interface ISchemaStyleProps extends WithStyles<keyof ISchemaStyles> {}
+
+export interface ISchemaProps {
+    style?: React.CSSProperties;
+    className?: string;
+    config: IFormConfig;
+    onConfig(config: IFormConfig): void;
+}
+
+export interface ISchemaStates {
+    config: string;
+}
