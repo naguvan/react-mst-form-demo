b4b79f4f3b780acb3a1fd76cd6c81a7d49aede33
naguvan <naguvan@outlook.com>
2018-09-25 01:12:51 +0530

    react-mst-form 1.0.10 update

--- src/Schema.tsx
+++ src/Schema.tsx
@@ -14,28 +14,34 @@ export class Schema extends Component<
   ISchemaProps & ISchemaStyleProps,
   ISchemaStates
 > {
-  constructor(props: ISchemaProps & ISchemaStyleProps) {
-    super(props);
-    this.state = Schema.getState(props);
-  }
-
-  static getState(props: ISchemaProps & ISchemaStyleProps): ISchemaStates {
+  static getDerivedStateFromPropsFix(
+    props: Readonly<ISchemaProps & ISchemaStyleProps>,
+    state?: ISchemaStates
+  ): ISchemaStates {
     const { config } = props;
     return { config: JSON.stringify(config, null, 2) };
   }
 
+  constructor(props: ISchemaProps & ISchemaStyleProps) {
+    super(props);
+    this.state = Schema.getDerivedStateFromPropsFix(props);
+  }
+
   componentWillReceiveProps(
-    nextProps: Readonly<ISchemaProps & ISchemaStyleProps>,
-    nextContext: any
+    nextProps: Readonly<ISchemaProps & ISchemaStyleProps>
   ): void {
-    this.setState(() => Schema.getState(nextProps));
+    if (this.props.config !== nextProps.config) {
+      this.setState(state =>
+        Schema.getDerivedStateFromPropsFix(nextProps, state)
+      );
+    }
   }
 
   public render(): ReactNode {
     const { className, classes, style } = this.props;
     const { config } = this.state;
     return (
-      <>
+      <div {...{ className, style }}>
         <h1>Schema</h1>
         <Paper square elevation={3} className={classes.paper}>
           <TextField
@@ -54,7 +60,7 @@ export class Schema extends Component<
         >
           Render
         </Button>
-      </>
+      </div>
     );
   }
 
